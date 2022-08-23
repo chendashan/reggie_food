7fc8e2068187665c268a40038ad1c4b04e430e2a
chendashan <cds7777@163.com>
2022-08-24 00:00:00 +0800

    购物车

---
+++ src/main/java/com/example/reggie/controller/ShoppingCartController.java
@@ -0,0 +1,108 @@
+package com.example.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.reggie.common.BaseContext;
+import com.example.reggie.common.R;
+import com.example.reggie.entity.ShoppingCart;
+import com.example.reggie.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/shoppingCart")
+@Slf4j
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        //设置用户id, 指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        //查询当前菜品或者套餐是否在购物车中
+        Long dishId = shoppingCart.getDishId();
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
+
+        if (dishId != null) {
+            //添加到购物车中的是菜品
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            //添加到购物车中的是套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+
+        //查询当前菜品或者套餐是否在购物车中
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+
+        if (cartServiceOne != null) {
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+
+
+        return R.success(cartServiceOne);
+    }
+
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list() {
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        queryWrapper.orderByAsc(ShoppingCart::getCreateTime);
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+
+        return R.success(list);
+    }
+
+    @DeleteMapping("/clean")
+    public R<String> clean() {
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        shoppingCartService.remove(queryWrapper);
+
+        return R.success("清空购物车成功");
+    }
+
+    @PostMapping("/sub")
+    public R<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart) {
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        if (shoppingCart.getDishId() != null) {
+            //添加到购物车中的是菜品
+            queryWrapper.eq(ShoppingCart::getDishId, shoppingCart.getDishId());
+        } else {
+            //添加到购物车中的是套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+
+        if (cartServiceOne == null) {
+            return null;
+        }
+
+        cartServiceOne.setNumber(cartServiceOne.getNumber() - 1);
+        if (cartServiceOne.getNumber() <= 0) {
+            shoppingCartService.remove(queryWrapper);
+        } else {
+            shoppingCartService.updateById(cartServiceOne);
+        }
+
+        return R.success(cartServiceOne);
+    }
+
+
+}
